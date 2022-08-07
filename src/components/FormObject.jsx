cfe6334b6b71502cf512d99e7a9ec76390f9bab5
fatihh67-basarir <fatih.basarir67@gmail.com>
2022-08-07 15:40:11 +0300

    change v1

--- src/components/FormObject.jsx
+++ src/components/FormObject.jsx
@@ -2,23 +2,28 @@ import { useState } from "react"
 
 
 const FormObject = () => {
-  const [username, setUsername] = useState("")
-  const [email, setEmail] = useState("")
-  const [password, setPassword] = useState ("")
+  const [formValues, setFormValues] = useState({
+    username:"", 
+    email:"" , 
+    password:""
+});
 
-  const handleUsername = (e) => {
-    console.log(e.target.value);
-    setUsername(e.target.value);
-  }
+const handleFormValues = (e) => {
+    // console.log(e.target.name);
+    // console.log(e.target.value);
+    // console.log(e.target.id);
 
+    setFormValues({...formValues, [e.target.id]: e.target.value })
+
+}
+  
   const handleSubmit = (e) => {
-    e.preventDefault();
+    // e.preventDefault();
+    const {username,password,email} = formValues;
     alert(`username:${username}
     email:${email}
     password:${password}
-    `);
-
-    
+    `);  
   }
 
   
@@ -26,17 +31,17 @@ const FormObject = () => {
     <div >
         <form onSubmit={handleSubmit}>
   <div className="mb-3 ">
-    <label htmlFor="username" className="form-label">Username <span>{username}</span></label>
-    <input type="text" className="form-control" id="username" value={username} onChange={handleUsername}/>
+    <label htmlFor="username" className="form-label">Username <span>{formValues.username}</span></label>
+    <input type="text" className="form-control" id="username" value={formValues.username} onChange={handleFormValues}/>
   </div>       
 
   <div className="mb-3 ">
-    <label htmlFor="email" className="form-label">Email address <span>{email}</span></label>
-    <input type="email" className="form-control" id="email" value={email} onChange={(e) => setEmail(e.target.value)} />
+    <label htmlFor="email" className="form-label">Email address <span>{formValues.email}</span></label>
+    <input type="email" className="form-control" id="email" value={formValues.email} onChange={handleFormValues} />
   </div>
   <div className="mb-3">
     <label htmlFor="password" className="form-label">Password</label>
-    <input type="password" className="form-control" id="password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+    <input type="password" className="form-control" id="password" value={formValues.password} onChange={handleFormValues}/>
   </div>
   <button  type="submit" className="btn btn-primary">Submit</button>
 </form>
