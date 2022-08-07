69583fe64b3caad991944aed02a1098f69b6ce58
fatihh67-basarir <fatih.basarir67@gmail.com>
2022-08-07 23:24:09 +0300

    change v2

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 
 import './App.css';
 import Form from './components/1-Forms/Form';
+import FormObject from './components/1-Forms/FormObject';
 import KeyboardClipboard from './components/2-keyboardClipboardEvents/KeyboardClipboard';
-import FormObject from './components/FormObject';
+import MouseEvent from './components/3-MouseEvents/MouseEvent';
+
 
 function App() {
   return (
     <div className="app">
       {/* <Form/> */}
     {/* <FormObject/> */}
-    <KeyboardClipboard/>
+    {/* <KeyboardClipboard/> */}
+    <MouseEvent/>
     </div>
   );
 }
