4250d6c0ca5584e0e2197236cf844bfac72306ec
fatihh67-basarir <fatih.basarir67@gmail.com>
2022-08-07 16:33:22 +0300

    Keyboard done

--- src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
+++ src/components/2-keyboardClipboardEvents/KeyboardClipboard.jsx
@@ -11,12 +11,27 @@ const KeyboardClipboard = () => {
         e.keyCode === 13 && alert("Enter submit")
 
     }
+
+    const handleAreaPaste = (e) =>{
+        e.target.value = e.clipboardData.getData("text").toLowerCase();
+        e.target.style.border = "3px solid red";
+        e.target.style.backgroundColor = "green"
+        e.preventDefault();
+    }
+
+    const handleAreaChange = (e) => {
+        if(e.target.value){
+            e.target.style.border = "1px solid black";
+        e.target.style.backgroundColor = "white"
+        }
+    }
   return (
     <div >
         <h2>Clickboard Event</h2>
         <input type="text" value={inputValue} onChange={(e) => setInputValue(e.target.value)} onKeyDown={handleKeyDown} />
 
-        <p>{inputValue.toUpperCase()}</p>
+        <p onCopy={() => alert("Dikkat Kopyalandı")}>{inputValue.toUpperCase()}</p>
+        <textarea name="area" id="area" cols="50" rows="10" onPaste={handleAreaPaste} onChange={handleAreaChange}></textarea>
     </div>
   )
 }
